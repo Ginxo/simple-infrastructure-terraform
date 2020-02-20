7517c11c730c3ac74f79c17d5f0ceefdba77340c
Alberto Martinez <amartlop@everis.com>
2020-02-20 19:01:56 +0100

    add todo in order to add a lock for terraform changes in some machines

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "sercore-terraform"
     key = "staging"
     region = "eu-west-1"
+    // TODO create TerraformLock DynamoDb
+    //dynamodb_table = "TerraformLock"
   }
 }
 
