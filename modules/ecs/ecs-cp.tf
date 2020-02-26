2e702ae86c451dfc0fdd11c3d21f8e874190367b
Alberto Martinez <albertoml.ohara@gmail.com>
2020-02-26 20:16:42 +0100

    change name of capacity provider and set port in lc template

--- modules/ecs/ecs-cp.tf
+++ modules/ecs/ecs-cp.tf
@@ -1,8 +1,12 @@
 resource "aws_ecs_capacity_provider" "ecs-cp" {
-  name = "${var.environment}-ecs-cp"
+  name = "${var.environment}-ecs-capacity-provider"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.web.arn
     managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      status = "DISABLED"
+    }
   }
 }
