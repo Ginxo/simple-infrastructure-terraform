af73b8999cb439f0729a7b597670ddc91171674f
Alberto Martinez <albertoml.ohara@gmail.com>
2020-02-28 19:29:29 +0100

    add to user resources the user integration

--- modules/api/api.tf
+++ modules/api/api.tf
@@ -3,21 +3,6 @@ name = "${var.environment}-api-gateway"
 description = "Proxy to handle requests to our API"
 }
 
-resource "aws_api_gateway_integration" "integration_api_users_endpoints" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource_api_users_endpoints.id
-  http_method = aws_api_gateway_method.method_api_users_endpoints.http_method
-  connection_type = "VPC_LINK"
-  connection_id = aws_api_gateway_vpc_link.vpc_link.id
-  integration_http_method = "ANY"
-  type                    = "HTTP_PROXY"
-  uri                     = "http://${var.lb_dns}:8080/api/users/{proxy}"
-
-  request_parameters =  {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
-}
-
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [aws_api_gateway_integration.integration_api_users_endpoints]
   rest_api_id = aws_api_gateway_rest_api.api.id
