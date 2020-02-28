a9b5f20634885182aa20b76ba7400d344eb60f2d
Alberto Martinez <albertoml.ohara@gmail.com>
2020-02-28 17:11:37 +0100

    add security group to rds

--- modules/rds/db_instance.tf
+++ modules/rds/db_instance.tf
@@ -9,5 +9,7 @@ resource "aws_db_instance" "rds-db" {
   password = var.db_password
   publicly_accessible = false
   skip_final_snapshot = true
-  
+  vpc_security_group_ids = [
+    aws_security_group.allow_rds_connection.id
+  ]
 }
