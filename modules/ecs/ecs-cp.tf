0131550a2c6756fbd17883bf339db8adbead4263
fetilla <fetilla@gmail.com>
2020-02-28 17:15:06 +0100

    :bug: increasing name ecs-cp

--- modules/ecs/ecs-cp.tf
+++ modules/ecs/ecs-cp.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_capacity_provider" "ecs-cp" {
-  name = "${var.environment}-ecs-capacity-provider"
+  name = "${var.environment}-ecs-capacity-provider-1"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.web.arn
