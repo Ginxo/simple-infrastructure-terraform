f46178312e4210b2c1d8d99d4485b601dddbd462
fetilla <fetilla@gmail.com>
2020-02-28 19:27:11 +0100

    :recycle: moved to used resources

--- modules/api/user-resources.tf
+++ modules/api/user-resources.tf
@@ -19,3 +19,18 @@ resource "aws_api_gateway_method" "method_api_users_endpoints" {
     "method.request.path.proxy" = true
   }
 }
+
+resource "aws_api_gateway_integration" "integration_api_users_endpoints" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_api_users_endpoints.id
+  http_method = aws_api_gateway_method.method_api_users_endpoints.http_method
+  connection_type = "VPC_LINK"
+  connection_id = aws_api_gateway_vpc_link.vpc_link.id
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${var.lb_dns}:8080/api/users/{proxy}"
+
+  request_parameters =  {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
