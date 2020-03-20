019543c72531125d295652fb1964c2ceda22f7b7
Alberto Martinez <albertoml.ohara@gmail.com>
2020-03-20 14:22:20 +0100

    delete stage item because the deployment creates the stage if not exist

--- modules/api/api.tf
+++ modules/api/api.tf
@@ -4,13 +4,9 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [aws_api_gateway_integration.integration_api_users_endpoints]
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = var.environment
+
+  depends_on = [aws_api_gateway_integration.integration_api_users_endpoints]
 }
 
-resource "aws_api_gateway_stage" "api_stage" {
-  stage_name    = var.environment
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
-}
